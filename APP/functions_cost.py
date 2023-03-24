74f09a15bf843f613c9d51fd6c1256337d1413ea
nohoraespana20 <nohora.espana@gmail.com>
2023-03-24 15:33:12 -0500

    Last version app

--- APP/functions_cost.py
+++ APP/functions_cost.py
@@ -71,7 +71,24 @@ def costEquation(CenPeak,  CenPeakOff):
     Ctco = np.reshape(Ctco, (3,3))
     return Ctco
 
-def accumulatedCost2(initialCost_C, initialCost_E, currency, year, yearlyRaise_C,yearlyRaise_E,IPC,yearlyRaise_batery,yearlyRaise_others, Cen_C, Cen_E, Cm_C, Cm_E, othersC, othersE, otherC, otherE, Ec, Ee, batery_capacity):
+# def accumulatedCost2(initialCost_C, initialCost_E, currency, year, yearlyRaise_C,yearlyRaise_E,IPC,yearlyRaise_batery,yearlyRaise_others, Cen_C, Cen_E, Cm_C, Cm_E, othersC, othersE, otherC, otherE, Ec, Ee, batery_capacity):
+def accumulatedCost2(initialCost_C, yearlyRaise_C, Cen_C, Cm_C, othersC, otherC, yearlyRaise_others,
+                     initialCost_E, yearlyRaise_E, yearlyRaise_batery, Cen_E, Cm_E, othersE, otherE, batery_capacity, 
+                     currency, year, IPC, Ec):
+    
+    initialCost_C =  initialCost_C / currency
+    Cm_C = Cm_C / currency 
+    Cen_C = Cen_C / currency
+    othersC = othersC / currency
+    otherC = otherC / currency
+
+    initialCost_E = initialCost_E / currency
+    Cen_E = Cen_E / currency
+    Cm_E = Cm_E / currency
+    othersE = otherE / currency
+    otherE = otherE / currency
+
+    Ee = Ec * 1.16
     totalC = []
     totalE = []
     totalC = [*range(0, year, 1)]
@@ -79,7 +96,7 @@ def accumulatedCost2(initialCost_C, initialCost_E, currency, year, yearlyRaise_C
     totalC[0] = initialCost_C
     totalE[0] = initialCost_E
     #capacidad bateria: 53.6 kWh # USD 156/kWh # tasa de cambio dolar 4997.9 # USD cambio a millones de pesos
-    bateryCost = 53.6 * batery_capacity * 4997.9 / currency 
+    bateryCost = batery_capacity * 156 * 4997.9 / currency 
     for i in range(1,year,1):
         Cen_C = Cen_C + (Cen_C * yearlyRaise_C)
         Cen_E = Cen_E + (Cen_E * yearlyRaise_E)
@@ -124,61 +141,3 @@ def saveFigure(totalC, totalE, i, E, name):
     plt.grid()
     plt.savefig('data_files/%s.png' %name)
     plt.close()
-
-
-# USD = 1000000
-# year = 10
-# costGalonFuel = 8932 / USD
-# costkWh = 567.34 / USD
-# yearlyRaise_C = 0.0712
-# yearlyRaise_E = 0.0660
-
-# costMaintenance_C = 7227970/ USD
-# costMaintenance_E = costMaintenance_C * 0.35
-
-# yearlyRaise_others = 0.1
-# yearlyRaise_batery = -0.0967
-
-# IPC = 0.0434
-
-# initialCost_C =  65000000 / USD
-# initialCost_E = 145000000 / USD
-
-# SOAT_C = 285100  / USD
-# tax_C = initialCost_C * 0.01
-# otherC = 255682  / USD
-
-# SOAT_E = SOAT_C * 0.9
-# tax_E = initialCost_E * 0.01 * 0.4
-# otherE = otherC * 0.7
-
-# othersC = SOAT_C + tax_C 
-# othersE = SOAT_E + tax_E
-
-# file_C = 'data_files/Fuel_mean.json'
-# file_E = 'data_files/kWh_mean.json'
-
-# #Costo energético promedio por kilómetro - combustion
-# CenPeak_C,  CenPeakOff_C =  meanFuelPerKM_C(file_C)
-# CenPeak_C = CenPeak_C * costGalonFuel
-# CenPeakOff_C =  CenPeakOff_C * costGalonFuel
-# Cen_C = (CenPeak_C + CenPeakOff_C)/2
-
-# #Costo energético promedio por kilómetro - electrico
-# CenPeak_E,  CenPeakOff_E =  meanFuelPerKM_E(file_E)
-# CenPeak_E = CenPeak_E * costkWh
-# CenPeakOff_E =  CenPeakOff_E * costkWh
-# Cen_E = (CenPeak_E + CenPeakOff_E)/2
-
-# Ec1 = 29353;  Ec2 = 47447;  Ec3 = 94152;  Ec4 = 45406
-# Ee1 = 33987; Ee2 = 54938; Ee3 = 69142; Ee4 = 46593
-
-# totalC_E12, totalE_E12, i = accumulatedCost2(Cen_C, Cen_E, costMaintenance_C, costMaintenance_E, othersC, othersE, otherC, otherE, Ec1, Ee1)
-# totalC_E22, totalE_E22, i = accumulatedCost2(Cen_C, Cen_E, costMaintenance_C, costMaintenance_E, othersC, othersE, otherC, otherE, Ec2, Ee2)
-# totalC_E32, totalE_E32, i = accumulatedCost2(Cen_C, Cen_E, costMaintenance_C, costMaintenance_E, othersC, othersE, otherC, otherE, Ec3, Ee3)
-# totalC_E42, totalE_E42, i = accumulatedCost2(Cen_C, Cen_E, costMaintenance_C, costMaintenance_E, othersC, othersE, otherC, otherE, Ec4, Ee4)
-
-# saveFigure(totalC_E12, totalE_E12, i, Ec1, 'E1_maintenance')
-# saveFigure(totalC_E22, totalE_E22, i, Ec2, 'E2_maintenance')
-# saveFigure(totalC_E32, totalE_E32, i, Ec3, 'E3_maintenance')
-# saveFigure(totalC_E42, totalE_E42, i, Ec4, 'E4_maintenance')
