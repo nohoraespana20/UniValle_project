e35218cf56f31cb174246a87d16efab0b48eeb39
nohoraespana20 <nohora.espana@gmail.com>
2024-04-02 16:36:13 -0500

    Functions to calculate comsumption and autonomy for EV and ICE were validated

--- pasto_case/calculate_metrics.py
+++ pasto_case/calculate_metrics.py
@@ -20,7 +20,7 @@ def total_per_trip(data, route, emission_class):
 
 def generate_data_frame(emission_classes, file):
     '''
-    Organize data in a data frame
+    Organize vehicles data in a data frame
     '''
     data = pd.read_csv(file)
     routes = [1, 2, 3, 4, 5, 6]
@@ -31,27 +31,39 @@ def generate_data_frame(emission_classes, file):
             total_route = total_per_trip(data, route, emission)
             total_data.append(total_route)
     total_data_frame = pd.DataFrame(total_data,columns=category)
-    data_frame = total_data_frame.iloc[:, [0,1,3,4,5,6,7,8,9,10]]
+    data_frame = total_data_frame.iloc[:, :]#[0,1,3,4,5,6,7,8,9,10]]
     return data_frame
 
 def consumption_metric(vehType, distance, consumption):
+    '''
+    Efficiency is quantified in terms of consumption per kilometer traveled 
+    vehType: EV (Electric Vehicle) or ICE (Internal Combustion Engine)
+    distance: paths in km
+    consumption: fuel or kwh consumed in paths
+    '''
     if vehType == "ICE":
         E_100km = (consumption*3.785*10.7*100)/distance
     elif vehType == "EV":
         E_100km = (consumption*100)/distance
     else:
         print('Vehicle type is not defined')
-    return E_100km
+    return round(E_100km, 2)
 
 def autonomy_metric(vehType, E_100km, capacity):
+    '''
+    Return driving range in km.
+    vehType: EV (Electric Vehicle) or ICE (Internal Combustion Engine)
+    E_100km: Efficiency is quantified in terms of consumption per 100 kilometer traveled
+    capacity: tank (gallons) or battery (kWh) capacity depends from vehType
+    '''
     if vehType == "ICE":
         consumption = (E_100km/(100*3.785*10.7))
         autonomy = capacity / consumption
     elif vehType == "EV":
-        autonomy = capacity / (E_100km/100)
+        autonomy = capacity * 0.85 / (E_100km/100)
     else:
         print('Vehicle type is not defined')
-    return autonomy
+    return round(autonomy, 2)
 
 def ICR_metric(powerCost, consumption, distance):
     icr = powerCost * consumption / distance
@@ -64,18 +76,66 @@ def CA_metric(years, maintenance, insurance, tax, tm_inspection, consumption):
         CA    
     return CA
 
+def mean_daily(vehType, dataFrame, numberPaths, category):
+    '''
+    Calculate the mean daily for all categories in vehicle data.
+    vehType: EV (Electric Vehicle) or ICE (Internal Combustion Engine)
+    dataFrame: local variable for data frame from vehicle data
+    numberPaths: number of paths done daily
+    category: number corresponding to list of categories [0:'route', 1:'emission_class',
+    2:'distance [km]', 3:'CO2 [kg]', 4:'CO [kg]', 5:'HC [kg]', 6:'PMx [kg]', 7:'NOx [kg]', 
+    8:'fuel [gl]', 9:'energy [kWh]', 10:'noise [dB]']
+    '''
+    np_np = numberPaths * 0.5
+    np_p = numberPaths * 0.3
+    p_p = numberPaths * 0.2
+
+    if vehType == "ICE":
+        route1_tot = list(dataFrame.iloc[0:5, category])
+        route2_tot = list(dataFrame.iloc[10:15, category])
+        route3_tot = list(dataFrame.iloc[25:30, category])
+
+        route1 = (route1_tot[0]*0.25)+(route1_tot[0]*0.20)+(route1_tot[0]*0.20)+(route1_tot[0]*0.20)+(route1_tot[0]*0.15)
+        route2 = (route2_tot[0]*0.25)+(route2_tot[0]*0.20)+(route2_tot[0]*0.20)+(route2_tot[0]*0.20)+(route2_tot[0]*0.15)
+        route3 = (route3_tot[0]*0.25)+(route3_tot[0]*0.20)+(route3_tot[0]*0.20)+(route3_tot[0]*0.20)+(route3_tot[0]*0.15)
+
+    elif vehType == "EV":
+        route1 = dataFrame.iloc[0, category]
+        route2 = dataFrame.iloc[2, category]
+        route3 = dataFrame.iloc[5, category]
+    else:
+        print('Vehicle type is not defined')
+    mean = route1*np_np + route2*np_p + route3*p_p
+    return round(mean,2)
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import pandas as pd
+    import numpy as np
 
     #Generate data frame for EV
     emission_classes_EV = ['Energy/unknown']
     rush_df_EV = generate_data_frame(emission_classes_EV,"./results/rush/data_emissions_EV.csv")
     offPeak_df_EV = generate_data_frame(emission_classes_EV,"./results/off_peak/data_emissions_EV.csv")
 
-    #Import SUMO data for ICE
+    #Generate data frame for ICE
     emission_classes_ICE = ['HBEFA4/PC_petrol_Euro-2', 'HBEFA4/PC_petrol_Euro-3', 
                         'HBEFA4/PC_petrol_Euro-4', 'HBEFA4/PC_petrol_Euro-5', 
                         'HBEFA4/PC_petrol_Euro-6d']
     rush_df_ICE = generate_data_frame(emission_classes_ICE,"./results/rush/data_emissions_ICE.csv")
     offPeak_df_ICE = generate_data_frame(emission_classes_ICE,"./results/off_peak/data_emissions_ICE.csv")
+    
+
+    #category=[0:'route', 1:'emission_class', 2:'distance [km]', 3:'CO2 [kg]', 4:'CO [kg]', 5:'HC [kg]', 6:'PMx [kg]', 7:'NOx [kg]', 8:'fuel [gl]', 9:'energy [kWh]', 10:'noise [dB]']
+
+    #Calculate the consumption metric - E100km - Rush hour
+    print('RUSH HOUR\n')
+    E100km_ICE = consumption_metric('ICE', mean_daily('ICE', rush_df_ICE, 35, 2), mean_daily('ICE', rush_df_ICE, 35, 8))
+    E100km_EV = consumption_metric('EV', mean_daily('EV', rush_df_EV, 35, 2), mean_daily('EV', rush_df_EV, 35, 9))
+
+    print('E100km_ICE [kWh/100km] = ', E100km_ICE, '\nE100km_EV [kWh/100km] = ', E100km_EV)
+
+    autonomy_ICE = autonomy_metric('ICE', E100km_ICE, 9.35) #View KIA grand EKO Taxi datasheet (tank capacity)
+    autonomy_EV = autonomy_metric('EV', E100km_EV, 53.5) #View BYD D1 datasheet (battery capacity)
+    print('Autonomy ICE [km]= ', autonomy_ICE, '\nAutonomy EV [km]= ', autonomy_EV)
