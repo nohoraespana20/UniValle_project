bf794be9d55ff58b2984570c30b2695793fda3f9
nohoraespana20 <nohora.espana@gmail.com>
2024-04-30 16:59:21 -0500

    Demand equation was integrated but no tested

--- pasto_case/projections.py
+++ pasto_case/projections.py
@@ -1,7 +1,9 @@
+import random
+
 def ice_to_ev(vehicleSeed, penetrationPercentage):
     evPenetration = []
     for i in range(len(penetrationPercentage)):
-        evPenetration.append(vehicleSeed * penetrationPercentage[i] / 100)
+        evPenetration.append(vehicleSeed * (i * 2) * penetrationPercentage[i] / 100)
     return evPenetration
 
 def annual_consumption_per_penetration(annualVehicles, annualConsumption):
@@ -14,17 +16,20 @@ def charging_time(chargeNeeded, chargingSpeed):
     hourChargingTime = chargeNeeded / chargingSpeed
     return round(hourChargingTime, 2)
 
-if __name__ == '__main__':
-    import pandas as pd
-    import calculate_metrics as cm
-    import matplotlib.pyplot as plt
-    from matplotlib.figure import Figure
-    
-    import numpy as np
-    import json
+def demand(C , P, T, N):
+    demand = []
+    for i in range(N):
+        b = random.random()
+        Pref = random.random()
+        demand.append(C * b * Pref)
+    totalDemand = sum(demand)/ (P * T)
+    return totalDemand
 
-    penetrationPercentage = [1, 5, 10, 25, 50, 80, 100] 
-    vehicleSeed = 3016 # Number of taxis in Pasto
+if __name__ == '__main__':
+    import calculate_metrics as cm
+
+    penetrationPercentage = [0.5, 1, 1.5, 2.0, 3.0, 10, 25, 50, 80, 100] 
+    vehicleSeed = 3016 # Number of taxis in Pasto (year = 2024)
 
     annualVehicles = ice_to_ev(vehicleSeed, penetrationPercentage)
     print('Annual EV = ', annualVehicles)
@@ -35,6 +40,13 @@ if __name__ == '__main__':
     annualConsumptionTotal = annual_consumption_per_penetration(annualVehicles, annualConsumption)
     print('Annual demand kWh per penetration percentage = ', annualConsumptionTotal)
 
-    print('Hour charging per year L1 = ', charging_time(annualConsumptionTotal[0], 1)/24)
-    print('Hour charging per year L2 = ', charging_time(annualConsumptionTotal[0], 6)/24)
-    print('Hour charging per year L3 = ', charging_time(annualConsumptionTotal[0], 50)/24)
+    print('Hour charging per year L1 = ', charging_time(annualConsumptionTotal[2], 1.9)/24)
+    print('Hour charging per year L2 = ', charging_time(annualConsumptionTotal[2], 7)/24)
+    print('Hour charging per year L3 = ', charging_time(annualConsumptionTotal[2], 50)/24)
+
+    C = 53.5 * cm.mean_daily('EV', dataframe, 35, 2) / 417.20
+    P = 11
+    T = 24
+    print(demand(C , 11, T, 3016))
+    print(demand(C , 22, T, 3016))
+    print(demand(C , 50, T, 3016))
