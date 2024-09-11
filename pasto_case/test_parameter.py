ace5a97eb641c48346b30b2ea09fcb4ea32e8905
nohoraespana20 <nohora.espana@gmail.com>
2024-09-11 16:50:22 -0500

    Doper library test with batteries

--- pasto_case/test_parameter.py
+++ pasto_case/test_parameter.py
@@ -97,8 +97,106 @@ def default_parameter():
     parameter['objective']['weight_load_shed'] = 1 # Weight of shed load costs ($/kWh)  in objective
     return parameter
 
-# Example data for running MULTI-NODE models (development)
-def parameter_add_network(parameter=None):
+# Timeseries Input Example Creation Funcs
+def ts_inputs(parameter={}, load='Flexlab', scale_load=4, scale_pv=4):
+    """ts_inputs"""
+    #scale = 1
+    #scale = 30
+    if load == 'Flexlab':
+        data = pd.read_csv(os.path.join(root_dir, 'ExampleData', 'Flexlab.csv')) # type: ignore
+        data.index = pd.to_datetime(data['Date/Time'].apply(lambda x: '2018/'+x[1:6]+' '+'{:2d}'.format(int(x[8:10])-1)+x[10::]))
+        del data.index.name
+        data = data[['FLEXLAB-X3-ZONEA:Zone Air Heat Balance System Air Transfer Rate [W](Hourly)']]
+        data.columns = ['load_demand']
+        data['load_demand'] = data['load_demand'].mask(data['load_demand']<0, data['load_demand']*-1/2.) / 1000.
+        # Use only 1 day
+        data = data.iloc[0:24]
+        data['load_demand'] = data['load_demand']/data['load_demand'].max()
+    elif load =='B90':
+        data = pd.DataFrame(index=pd.date_range(start='2019-01-01 00:00', end='2019-01-01 23:50', freq='h'))
+        # data['load_demand'] = [2.8,  2.8,  2.9,  2.9,  3. ,  3.3,  4. ,  4.8,  4.9,  5.1,  5.3,
+        #                        5.4,  5.4,  5.4,  5.3,  5.3,  5.2,  4.8,  3.9,  3.1,  2.9,  2.8,
+        #                        2.8,  2.8]
+        data['load_demand'] = [1.0,  1.0,  1.1,  1.1,  1.2,  1.5,  2.1,  2.9,  3.0,  3.2,  3.4,
+                               3.5,  3.5,  3.5,  3.4,  3.4,  3.3,  2.9,  2.0,  1.2,  1.0,  1.0,
+                               1.0,  1.0]
+        data['load_demand'] = data['load_demand']/data['load_demand'].max()
+    # Scale Load data
+    data['load_demand'] = data['load_demand'] * scale_load
+    # Mode of OAT
+    data['oat'] = np.sin(data.index.view(np.int64)/(1e12*np.pi*4))*3 + 22
+    # Makeup Tariff
+    data['tariff_energy_map'] = 0
+    data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=8) & (data.index.hour<22), 1)
+    data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=12) & (data.index.hour<18), 2)
+    data['tariff_power_map'] = data['tariff_energy_map'] # Apply same periods to demand charge
+    data['tariff_energy_export_map'] = 0
+    
+    data['tariff_regup'] = data['tariff_power_map'] * 0.05 + 0.01
+    data['tariff_regdn'] = data['tariff_power_map'] * 0.01 + 0.01
+    data['battery_reg'] = 0
+    data['date_time'] = data.index
+    # Resample
+    if True:
+        data = data.resample('5min').asfreq()
+        for c in data.columns:
+            if c in ['load_demand','oat']:
+                data[c] = data[c].interpolate()
+            else:
+                data[c] = data[c].ffill()
+        data = data.loc['2019-01-01 00:00:00':'2019-01-02 00:00:00']
+    else:
+        data = data.loc['2019-01-01 00:00:00':'2019-01-02 00:00:00']
+    
+    var = pd.read_csv('pv_norm_pasto.csv') * scale_pv
+    var_single_column = var.iloc[5:282, 0]
+    
+    data['generation_pv'] = var_single_column.values
+    
+    # input timeseries indicating grid availability
+    data['grid_available'] = 1
+    data['fuel_available'] = 0
+
+    # input timeseries indicating grid availability
+    # data['grid_co2_intensity'] = 0.202 #kg/kWh
+    data['grid_co2_intensity'] = 0.16438 #kg/kWh
+    return data
+
+def parameter_add_battery(parameter=None):
+    """parameter_add_battery"""
+    if parameter is None:
+        # if no parameter given, load default
+        parameter = default_parameter()
+
+    # enable gensets
+    parameter['system']['battery'] = True
+
+    # Add genset options
+    parameter['batteries'] = [
+        {
+         'name':'libat01',
+        'capacity': 200,
+         'degradation_endoflife': 80,
+         'degradation_replacementcost': 6000.0,
+         'efficiency_charging': 0.96,
+         'efficiency_discharging': 0.96,
+         'nominal_V':  400,
+         'power_charge': 50,
+         'power_discharge': 50,
+         'maxS': 50,
+         'self_discharging': 0.0,
+          'soc_final': None,
+         'soc_initial': 0.65,
+         'soc_max': 1,
+         'soc_min': 0.2,
+         # 'temperature_initial': 22.0,
+         'thermal_C': 100000.0,
+         'thermal_R': 0.01
+        }
+    ]
+    return parameter
+
+def parameter_add_network_test(parameter=None):
     """parameter_add_network"""
     if parameter is None:
         # if no parameter given, load default
@@ -107,35 +205,8 @@ def parameter_add_network(parameter=None):
     parameter['network'] = {}
     parameter['network']['settings'] = {
         # turn off simepl power exchange to utilize full power-flow equations
-        'simplePowerExchange': False,
+        'simplePowerExchange': True,
         'simpleNetworkLosses': 0.05,
-
-        # powerflow parameters
-        'slackBusVoltage': 1,
-        'sBase': 1,
-        'vBase': 1,
-        'cableDerating': 1,
-        'txDerating': 1,
-
-        # power factors
-        'powerFactors': {
-            'pv': 1,  
-            'genset': 1,
-            'batteryDisc': 1,
-            'batteryChar': 1,
-            'load': 1
-        },
-
-        # powerflow model settings
-        'enableLosses': True,
-        'thetaMin': -0.18,
-        'thetaMax': 0.09,
-        'voltMin': 0.8,
-        'voltMax': 1.1,
-        'useConsVoltMin': False,
-        'enableConstantPf': 1,
-        'enableVoltageAngleConstraint': 1,
-        'enableGenPqLimits': False, # not implemented yet
     }
 
     parameter['network']['nodes'] = [ # list of dict to define inputs for each node in network
@@ -278,169 +349,11 @@ def parameter_add_network(parameter=None):
     ]
     return parameter 
 
-# Timeseries Input Example Creation Funcs
-def ts_inputs(parameter={}, load='Flexlab', scale_load=4, scale_pv=4):
-    """ts_inputs"""
-    #scale = 1
-    #scale = 30
-    if load == 'Flexlab':
-        data = pd.read_csv(os.path.join(root_dir, 'ExampleData', 'Flexlab.csv'))
-        data.index = pd.to_datetime(data['Date/Time'].apply(lambda x: '2018/'+x[1:6]+' '+'{:2d}'.format(int(x[8:10])-1)+x[10::]))
-        del data.index.name
-        data = data[['FLEXLAB-X3-ZONEA:Zone Air Heat Balance System Air Transfer Rate [W](Hourly)']]
-        data.columns = ['load_demand']
-        data['load_demand'] = data['load_demand'].mask(data['load_demand']<0, data['load_demand']*-1/2.) / 1000.
-        # Use only 1 day
-        data = data.iloc[0:24]
-        data['load_demand'] = data['load_demand']/data['load_demand'].max()
-    elif load =='B90':
-        data = pd.DataFrame(index=pd.date_range(start='2019-01-01 00:00', end='2019-01-01 23:50', freq='h'))
-        # data['load_demand'] = [2.8,  2.8,  2.9,  2.9,  3. ,  3.3,  4. ,  4.8,  4.9,  5.1,  5.3,
-        #                        5.4,  5.4,  5.4,  5.3,  5.3,  5.2,  4.8,  3.9,  3.1,  2.9,  2.8,
-        #                        2.8,  2.8]
-        data['load_demand'] = [1.0,  1.0,  1.1,  1.1,  1.2,  1.5,  2.1,  2.9,  3.0,  3.2,  3.4,
-                               3.5,  3.5,  3.5,  3.4,  3.4,  3.3,  2.9,  2.0,  1.2,  1.0,  1.0,
-                               1.0,  1.0]
-        data['load_demand'] = data['load_demand']/data['load_demand'].max()
-    # Scale Load data
-    data['load_demand'] = data['load_demand'] * scale_load
-    # Mode of OAT
-    data['oat'] = np.sin(data.index.view(np.int64)/(1e12*np.pi*4))*3 + 22
-    # Makeup Tariff
-    data['tariff_energy_map'] = 0
-    data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=8) & (data.index.hour<22), 1)
-    data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=12) & (data.index.hour<18), 2)
-    data['tariff_power_map'] = data['tariff_energy_map'] # Apply same periods to demand charge
-    data['tariff_energy_export_map'] = 0
-    
-    data['tariff_regup'] = data['tariff_power_map'] * 0.05 + 0.01
-    data['tariff_regdn'] = data['tariff_power_map'] * 0.01 + 0.01
-    data['battery_reg'] = 0
-    data['date_time'] = data.index
-    # Resample
-    if True:
-        data = data.resample('5min').asfreq()
-        for c in data.columns:
-            if c in ['load_demand','oat']:
-                data[c] = data[c].interpolate()
-            else:
-                data[c] = data[c].ffill()
-        data = data.loc['2019-01-01 00:00:00':'2019-01-02 00:00:00']
-    else:
-        data = data.loc['2019-01-01 00:00:00':'2019-01-02 00:00:00']
-    
-    var = pd.read_csv('pv_norm_pasto.csv') * scale_pv
-    var_single_column = var.iloc[5:282, 0]
-    
-    data['generation_pv'] = var_single_column.values
-    
-    # input timeseries indicating grid availability
-    data['grid_available'] = 1
-    data['fuel_available'] = 0
-
-    # input timeseries indicating grid availability
-    # data['grid_co2_intensity'] = 0.202 #kg/kWh
-    data['grid_co2_intensity'] = 0.16438 #kg/kWh
-    return data
-
-def parameter_add_loadcontrol(parameter=None):
-    """parameter_add_loadcontrol"""
+def parameter_add_battery_multinode(parameter=None):
+    """parameter_add_battery_multinode"""
     if parameter is None:
         # if no parameter given, load default
-        parameter = default_parameter()
-
-    # enable gensets
-    parameter['system']['load_control'] = True
-
-    # Add genset options
-    parameter['load_control'] = [
-        {
-            'name': 'a',
-            'cost': 0.05, # $/kWh not served
-            'outageOnly': False
-        },
-        {
-            'name': 'b',
-            'cost': 0.3, # $/kWh not served
-            'outageOnly': False
-        }
-    ]
-    return parameter
-
-def parameter_add_loadcontrol_multinode(parameter=None):
-    """parameter_add_loadcontrol_multinode"""
-    if parameter is None:
-        # if no parameter given, load default
-        parameter = default_parameter()
-
-    # enable gensets
-    parameter['system']['load_control'] = True
-
-    # Add genset options
-    parameter['load_control'] = [
-        {
-            'name': 'testLc1A',
-            'cost': 0.25, # $/kWh not served
-            'outageOnly': False
-        },
-        {
-            'name': 'testLc1B',
-            'cost': 0.05, # $/kWh not served
-            'outageOnly': False
-        },
-        {
-            'name': 'testLc4',
-            'cost': 0.1, # $/kWh not served
-            'outageOnly': False
-        }
-    ]
-    return parameter
-
-def ts_inputs_load_shed(parameter, data=None):
-    """ts_inputs_load_shed"""
-    if data is None:
-        # load default data
-        data = ts_inputs(parameter, load='B90', scale_load=150, scale_pv=100)
-
-    # overwrite grid availability to disable grid connection
-    data['load_shed_potential_a'] = 0.3 * data['load_demand']
-    data['load_shed_potential_b'] = 0.15 * data['load_demand']
-    return data
-
-def ts_inputs_multinode_pf(parameter, data=None):
-    '''
-    func to create example 4-node load and pv profiles
-    '''
-    # create data ts for each node
-    data2 = ts_inputs(parameter, load='B90', scale_load=700, scale_pv=300)
-    data3 = ts_inputs(parameter, load='B90', scale_load=1200, scale_pv=1200)
-    data4 = ts_inputs(parameter, load='B90', scale_load=1500, scale_pv=1000)
-    data5 = ts_inputs(parameter, load='B90', scale_load=2000, scale_pv=1500)
-
-    # use data1 as starting point for multinode df
-    data = data2.copy()
-
-    # drop load and pv from multinode df
-    data = data.drop(labels='load_demand', axis=1)
-    data = data.drop(labels='generation_pv', axis=1)
-
-    # add node specifc load and pv (where applicable)
-    data['pf_demand_node2'] = data2['load_demand']
-    data['pf_demand_node3'] = data3['load_demand']
-    data['pf_demand_node4'] = data4['load_demand']
-    data['pf_demand_node5'] = data5['load_demand']
-
-    data['pf_pv_node2'] = data2['generation_pv']
-    data['pf_pv_node3'] = data3['generation_pv']
-    data['pf_pv_node4'] = data4['generation_pv']
-    data['pf_pv_node5'] = data5['generation_pv']
-    return data
-
-def parameter_add_battery(parameter=None):
-    """parameter_add_battery"""
-    if parameter is None:
-        # if no parameter given, load default
-        parameter = default_parameter()
+        parameter = parameter_add_network_test()
 
     # enable gensets
     parameter['system']['battery'] = True
@@ -448,24 +361,84 @@ def parameter_add_battery(parameter=None):
     # Add genset options
     parameter['batteries'] = [
         {
-         'name':'libat01',
-        'capacity': 200,
-         'degradation_endoflife': 80,
-         'degradation_replacementcost': 6000.0,
-         'efficiency_charging': 0.96,
-         'efficiency_discharging': 0.96,
-         'nominal_V':  400,
-         'power_charge': 50,
-         'power_discharge': 50,
-         'maxS': 50,
-         'self_discharging': 0.0,
-          'soc_final': None,
-         'soc_initial': 0.65,
-         'soc_max': 1,
-         'soc_min': 0.2,
-         # 'temperature_initial': 22.0,
-         'thermal_C': 100000.0,
-         'thermal_R': 0.01
-        }
+          'name':'pf_bat_node2',
+          'capacity': 500,
+          'degradation_endoflife': 80,
+          'degradation_replacementcost': 6000.0,
+          'efficiency_charging': 0.96,
+          'efficiency_discharging': 0.96,
+          'nominal_V':  400,
+          'power_charge': 150,
+          'maxS': 150,
+          'power_discharge': 150,
+          'self_discharging': 0.001,
+           'soc_final': 0.5,
+          'soc_initial': 0.5,
+          'soc_max': 1,
+          'soc_min': 0.2,
+          # 'temperature_initial': 22.0,
+          'thermal_C': 100000.0,
+          'thermal_R': 0.01
+        },
+        {
+          'name':'pf_bat_node3',
+          'capacity': 1000,
+          'degradation_endoflife': 80,
+          'degradation_replacementcost': 6000.0,
+          'efficiency_charging': 0.96,
+          'efficiency_discharging': 0.96,
+          'nominal_V':  400,
+          'power_charge': 350,
+          'maxS': 350,
+          'power_discharge': 350,
+          'self_discharging': 0.001,
+           'soc_final': 0.5,
+          'soc_initial': 0.5,
+          'soc_max': 1,
+          'soc_min': 0.2,
+          # 'temperature_initial': 22.0,
+          'thermal_C': 100000.0,
+          'thermal_R': 0.01
+        },
+        {
+          'name':'pf_bat_node4',
+          'capacity': 1000,
+          'degradation_endoflife': 80,
+          'degradation_replacementcost': 6000.0,
+          'efficiency_charging': 0.96,
+          'efficiency_discharging': 0.96,
+          'nominal_V':  400,
+          'power_charge': 350,
+          'maxS': 350,
+          'power_discharge': 350,
+          'self_discharging': 0.001,
+           'soc_final': 0.5,
+          'soc_initial': 0.5,
+          'soc_max': 1,
+          'soc_min': 0.2,
+          # 'temperature_initial': 22.0,
+          'thermal_C': 100000.0,
+          'thermal_R': 0.01
+        },
+        {
+          'name':'pf_bat_node5',
+          'capacity': 1000,
+          'degradation_endoflife': 80,
+          'degradation_replacementcost': 6000.0,
+          'efficiency_charging': 0.96,
+          'efficiency_discharging': 0.96,
+          'nominal_V':  400,
+          'power_charge': 350,
+          'maxS': 350,
+          'power_discharge': 350,
+          'self_discharging': 0.001,
+           'soc_final': 0.5,
+          'soc_initial': 0.5,
+          'soc_max': 1,
+          'soc_min': 0.2,
+          # 'temperature_initial': 22.0,
+          'thermal_C': 100000.0,
+          'thermal_R': 0.01
+        },
     ]
     return parameter
