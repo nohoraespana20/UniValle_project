01756402e1f8ef6337ac048a875dbd73518ee13a
nohoraespana20 <nohora.espana@gmail.com>
2024-10-08 15:55:53 -0500

    Test with tariff and demand curve adjusted

--- pasto_case/parameter_pasto.py
+++ pasto_case/parameter_pasto.py
@@ -57,10 +57,10 @@ def parameters():
         parameter['tariff']['demand_coincident'] = 0.5 # $/kW for coincident
         parameter['tariff']['export'] = {0:0} # $/kWh for periods 0-offpeak, 1-midpeak, 2-onpeak
     else:
-        parameter['tariff']['energy'] = {0:0.25, 1:0.25, 2:0.25} # $/kWh for periods 0-offpeak, 1-midpeak, 2-onpeak
-        parameter['tariff']['demand'] = {0:0.25, 1:0.25, 2:0.25} # $/kW for periods 0-offpeak, 1-midpeak, 2-onpeak
+        parameter['tariff']['energy'] = {0:0.25, 1:0.5, 2:0.5} # $/kWh for periods 0-offpeak, 1-midpeak, 2-onpeak
+        parameter['tariff']['demand'] = {0:0.25, 1:0.5, 2:0.5} # $/kW for periods 0-offpeak, 1-midpeak, 2-onpeak
         # parameter['tariff']['demand'] = {0:67.9, 1:67.9, 2:67.9} # $/kW for periods 0-offpeak, 1-midpeak, 2-onpeak
-        parameter['tariff']['demand_coincident'] = 0.25 # $/kW for coincident
+        parameter['tariff']['demand_coincident'] = 0.5 # $/kW for coincident
         parameter['tariff']['export'] = {0:0.12} # $/kWh for periods 0-offpeak, 1-midpeak, 2-onpeak
         # parameter['tariff']['export'] = {0:0.14} # $/kWh for periods 0-offpeak, 1-midpeak, 2-onpeak
 
@@ -101,7 +101,7 @@ def parameters():
     # Add network settings to define power-flow constaints
     parameter['network']['settings'] = {
         # turn off simepl power exchange to utilize full power-flow equations
-        'simplePowerExchange': True,
+        'simplePowerExchange': False,
         'simpleNetworkLosses': 0.05,
         # powerflow parameters
         'slackBusVoltage': 1,
@@ -133,7 +133,7 @@ def parameters():
         { # node 1 Nodo SE Pasto
             'node_id': 'N1', # unique str to id node
             'pcc': True, # bool to define if node is pcc
-            'slack': True, 
+            'slack': False, 
             'load_id': None, # str, list of str, or None to find load profile in ts data (if node is load bus) by column label
             'ders': { # dict of der assets at node, if None or not included, no ders present
                 'pv_id': None, # str, list, or None to find pv profile in ts data (if pv at node) by column label
@@ -208,7 +208,7 @@ def parameters():
         { # node 2 SE Catambuco
             'node_id': 'N2',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': None,
             'ders': { 
                 'pv_id': None,
@@ -328,7 +328,7 @@ def parameters():
         { # node 4 Udenar T
             'node_id': 'N4',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node4',
             'ders': { 
                 'pv_id': 'pf_pv_node4',
@@ -347,7 +347,7 @@ def parameters():
         { # node 5 H San Pedro
             'node_id': 'N5',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node5',
             'ders': { 
                 'pv_id': 'pf_pv_node5',
@@ -366,7 +366,7 @@ def parameters():
         { # node 6 Liceo Udenar
             'node_id': 'N6',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node6',
             'ders': { 
                 'pv_id': 'pf_pv_node6',
@@ -385,7 +385,7 @@ def parameters():
         { # node 7 Udenar Vipri
             'node_id': 'N7',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node7',
             'ders': { 
                 'pv_id': 'pf_pv_node7',
@@ -404,7 +404,7 @@ def parameters():
         { # node 8 Unimar Alvernia
             'node_id': 'N8',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node8',
             'ders': { 
                 'pv_id': 'pf_pv_node8',
@@ -423,7 +423,7 @@ def parameters():
         { # node 9 Unimar
             'node_id': 'N9',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node9',
             'ders': { 
                 'pv_id': 'pf_pv_node9',
@@ -442,7 +442,7 @@ def parameters():
         { # node 10 Alkosto C
             'node_id': 'N10',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node10',
             'ders': { 
                 'pv_id': 'pf_pv_node10',
@@ -461,7 +461,7 @@ def parameters():
         { # node 11 Cra 27 Cll 19-20
             'node_id': 'N11',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node11',
             'ders': { 
                 'pv_id': 'pf_pv_node11',
@@ -480,7 +480,7 @@ def parameters():
         { # node 12 Policia centro 
             'node_id': 'N12',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node12',
             'ders': { 
                 'pv_id': 'pf_pv_node12',
@@ -499,7 +499,7 @@ def parameters():
         { # node 13 Cra 27 Cll 17-18
             'node_id': 'N13',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node13',
             'ders': { 
                 'pv_id': 'pf_pv_node13',
@@ -518,7 +518,7 @@ def parameters():
         { # node 14 CC Sebastian
             'node_id': 'N14',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node14',
             'ders': { 
                 'pv_id': 'pf_pv_node14',
@@ -537,7 +537,7 @@ def parameters():
         { # node 15 Cll 17 Cra 25
             'node_id': 'N15',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node15',
             'ders': { 
                 'pv_id': 'pf_pv_node15',
@@ -556,7 +556,7 @@ def parameters():
         { # node 16 Comfamiliar
             'node_id': 'N16',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node16',
             'ders': { 
                 'pv_id': 'pf_pv_node16',
@@ -575,7 +575,7 @@ def parameters():
         { # node 17 Mariluz
             'node_id': 'N17',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node17',
             'ders': { 
                 'pv_id': 'pf_pv_node17',
@@ -594,7 +594,7 @@ def parameters():
         { # node 18 Terminal 1
             'node_id': 'N18',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node18',
             'ders': { 
                 'pv_id': 'pf_pv_node18',
@@ -613,7 +613,7 @@ def parameters():
         { # node 19 Alcaldia Anganoy
             'node_id': 'N19',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node19',
             'ders': { 
                 'pv_id': 'pf_pv_node19',
@@ -632,7 +632,7 @@ def parameters():
         { # node 20 Alcaldia Centro
             'node_id': 'N20',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node20',
             'ders': { 
                 'pv_id': 'pf_pv_node20',
@@ -651,7 +651,7 @@ def parameters():
         { # node 21 Terminal 2
             'node_id': 'N21',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node21',
             'ders': { 
                 'pv_id': 'pf_pv_node21',
@@ -670,7 +670,7 @@ def parameters():
         { # node 22 Terminal
             'node_id': 'N22',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node22',
             'ders': { 
                 'pv_id': 'pf_pv_node22',
@@ -689,7 +689,7 @@ def parameters():
         { # node 23 Cesmag
             'node_id': 'N23',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node23',
             'ders': { 
                 'pv_id': 'pf_pv_node23',
@@ -708,7 +708,7 @@ def parameters():
         { # node 24 Cesmag 1
             'node_id': 'N24',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node24',
             'ders': { 
                 'pv_id': 'pf_pv_node24',
@@ -727,7 +727,7 @@ def parameters():
         { # node 25 Cesmag 2
             'node_id': 'N25',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node25',
             'ders': { 
                 'pv_id': 'pf_pv_node25',
@@ -746,7 +746,7 @@ def parameters():
         { # node 26 Cesmag 3
             'node_id': 'N26',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node26',
             'ders': { 
                 'pv_id': 'pf_pv_node26',
@@ -765,7 +765,7 @@ def parameters():
         { # node 27 Sena 
             'node_id': 'N27',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node27',
             'ders': { 
                 'pv_id': 'pf_pv_node27',
@@ -784,7 +784,7 @@ def parameters():
         { # node 28 Ingenominas
             'node_id': 'N28',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node28',
             'ders': { 
                 'pv_id': 'pf_pv_node28',
@@ -801,28 +801,28 @@ def parameters():
             ]
         },
         { # node 29 Corponariño
-            'node_id': 'N28',
+            'node_id': 'N29',
             'pcc': True,
-            'slack': True, 
-            'load_id': 'pf_demand_node28',
+            'slack': False, 
+            'load_id': 'pf_demand_node29',
             'ders': { 
-                'pv_id': 'pf_pv_node28',
+                'pv_id': 'pf_pv_node29',
                 'pv_maxS': 199,
-                'battery': 'pf_bat_node28', # node can contain multiple battery assets, so should be list
+                'battery': 'pf_bat_node29', # node can contain multiple battery assets, so should be list
                 'genset': None,
                 'load_control': None # node likely to only contain single load_control asset, so should be str
             },
             'connections': [
                 {
                     'node': 'N3',
-                    'line': 'L28'
+                    'line': 'L29'
                 }
             ]
         },
         { # node 30 CC Unico
             'node_id': 'N30',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node30',
             'ders': { 
                 'pv_id': 'pf_pv_node30',
@@ -841,7 +841,7 @@ def parameters():
         { # node 31 H Departamental
             'node_id': 'N31',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node31',
             'ders': { 
                 'pv_id': 'pf_pv_node31',
@@ -860,7 +860,7 @@ def parameters():
         { # node 32 Alkosto PBolivar
             'node_id': 'N32',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node32',
             'ders': { 
                 'pv_id': 'pf_pv_node32',
@@ -879,7 +879,7 @@ def parameters():
         { # node 33 Edupar
             'node_id': 'N33',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node33',
             'ders': { 
                 'pv_id': 'pf_pv_node33',
@@ -898,7 +898,7 @@ def parameters():
         { # node 34 Buses Los Pinos
             'node_id': 'N34',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node34',
             'ders': { 
                 'pv_id': 'pf_pv_node34',
@@ -917,7 +917,7 @@ def parameters():
         { # node 35 Los Pinos
             'node_id': 'N35',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node35',
             'ders': { 
                 'pv_id': 'pf_pv_node35',
@@ -936,7 +936,7 @@ def parameters():
         { # node 36 Balcones La Carolina
             'node_id': 'N36',
             'pcc': True,
-            'slack': True, 
+            'slack': False, 
             'load_id': 'pf_demand_node36',
             'ders': { 
                 'pv_id': 'pf_pv_node36',
@@ -1921,11 +1921,10 @@ def ts_inputs(parameter={}, load='Flexlab', scale_load=4, scale_pv=4):
         data['load_demand'] = data['load_demand']/data['load_demand'].max()
     elif load =='B90':
         data = pd.DataFrame(index=pd.date_range(start='2019-01-01 00:00', end='2019-01-01 23:50', freq='h'))
-        # data['load_demand'] = [2.8,  2.8,  2.9,  2.9,  3. ,  3.3,  4. ,  4.8,  4.9,  5.1,  5.3,
-        #                        5.4,  5.4,  5.4,  5.3,  5.3,  5.2,  4.8,  3.9,  3.1,  2.9,  2.8,
-        #                        2.8,  2.8]
-        data['load_demand'] = [0.0, 0.0, 0.03, 0.03, 0.06, 0.14, 0.31, 0.54, 0.60, 0.69, 0.74,
-                                0.80, 0.80, 0.80, 0.74, 0.74, 0.69, 0.54, 0.31, 0.06, 0.0, 0.0, 0.0, 0.0]
+        # data['load_demand'] = [0.0, 0.0, 0.03, 0.03, 0.06, 0.14, 0.31, 0.54, 0.60, 0.69, 0.74,
+        #                         0.80, 0.80, 0.80, 0.74, 0.74, 0.69, 0.54, 0.31, 0.06, 0.0, 0.0, 0.0, 0.0]
+        data['load_demand'] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.03, 0.05, 0.25, 0.35, 0.5, 0.59, 
+                                0.52, 0.4, 0.35, 0.4, 0.6, 0.69, 0.8, 0.87, 0.75, 0.4, 0.1, 0.0]
         data['load_demand'] = data['load_demand']/data['load_demand'].max()
     # Scale Load data
     data['load_demand'] = data['load_demand'] * scale_load
@@ -1933,8 +1932,10 @@ def ts_inputs(parameter={}, load='Flexlab', scale_load=4, scale_pv=4):
     data['oat'] = np.sin(data.index.view(np.int64)/(1e12*np.pi*4))*3 + 22
     # Makeup Tariff
     data['tariff_energy_map'] = 0
-    data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=8) & (data.index.hour<22), 1)
-    data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=12) & (data.index.hour<18), 2)
+    # data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=8) & (data.index.hour<22), 1)
+    # data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=12) & (data.index.hour<18), 2)
+    data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=6) & (data.index.hour<9), 1)
+    data['tariff_energy_map'] = data['tariff_energy_map'].mask((data.index.hour>=17) & (data.index.hour<20), 2)
     data['tariff_power_map'] = data['tariff_energy_map'] # Apply same periods to demand charge
     data['tariff_energy_export_map'] = 0
     
--- pasto_case/powerFlow.py
+++ pasto_case/powerFlow.py
@@ -21,55 +21,46 @@ def control_model(inputs, parameter):
     model.objective = Objective(rule=objective_function, sense=minimize, doc='objective function')
     return model
 
-def data_multinode(parameter):
-    L2_demand = [8.9, 38.7, 582.5, 1155.6, 2989.8, 6340.0, 9108.8, 23724.3, 11865.5,
-             4629.7, 19548.7, 25972.5, 35441.8, 41558.6, 536.5, 2301.6, 139.3,
-             86847.3, 123598.7, 155795.6, 109602.5, 108342.6, 99047.5, 67926.1,
-             14709.8, 236133.7, 19783.1, 226017.5, 20115.6, 35732.1]
-    pv_per_node = [345, 125, 199, 2772, 594, 506, 866, 856, 1202, 137, 440, 688, 560,
-                 170, 480, 260, 215, 144, 249, 149, 185, 319]
-
-    data4  = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.04, scale_pv=861)
-    data5  = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.06, scale_pv=1202)
-    data6  = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=137)
-    data7  = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.02, scale_pv=440)
-    data8  = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.03, scale_pv=560)
-    data9  = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=170)
-    data10 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.02, scale_pv=480)
-    data11 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=260)
-    data12 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=215)
-    data13 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=146)
-    data14 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=249)
-    data15 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=151)
-    data16 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.02, scale_pv=319)
-    data17 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.05, scale_pv=934)
-    data18 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.03, scale_pv=594)
-    data19 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.03, scale_pv=688)
-    data20 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=185)
-    data21 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=147)
-    data22 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.10, scale_pv=1953)
-    data23 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.02, scale_pv=369)
-    data24 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=185)
-    data25 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=123)
-    data26 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=211)
-    data27 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.02, scale_pv=345)
-    data28 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=125)
-    data29 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.01, scale_pv=199)
-    data30 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.14, scale_pv=2772)
-    data31 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.02, scale_pv=506)
-    data32 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.04, scale_pv=866)
-    data33 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.06, scale_pv=1195)
-    data34 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.09, scale_pv=1831)
-    data35 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.02, scale_pv=449)
-    data36 = ts_inputs(parameter, load='B90', scale_load=L2_demand[6]*0.08, scale_pv=1594)
-
+def data_multinode(parameter, demand):
+    data4  = ts_inputs(parameter, load='B90', scale_load=demand*0.04, scale_pv=861)
+    data5  = ts_inputs(parameter, load='B90', scale_load=demand*0.06, scale_pv=1202)
+    data6  = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=137)
+    data7  = ts_inputs(parameter, load='B90', scale_load=demand*0.02, scale_pv=440)
+    data8  = ts_inputs(parameter, load='B90', scale_load=demand*0.03, scale_pv=560)
+    data9  = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=170)
+    data10 = ts_inputs(parameter, load='B90', scale_load=demand*0.02, scale_pv=480)
+    data11 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=260)
+    data12 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=215)
+    data13 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=146)
+    data14 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=249)
+    data15 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=151)
+    data16 = ts_inputs(parameter, load='B90', scale_load=demand*0.02, scale_pv=319)
+    data17 = ts_inputs(parameter, load='B90', scale_load=demand*0.05, scale_pv=934)
+    data18 = ts_inputs(parameter, load='B90', scale_load=demand*0.03, scale_pv=594)
+    data19 = ts_inputs(parameter, load='B90', scale_load=demand*0.03, scale_pv=688)
+    data20 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=185)
+    data21 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=147)
+    data22 = ts_inputs(parameter, load='B90', scale_load=demand*0.10, scale_pv=1953)
+    data23 = ts_inputs(parameter, load='B90', scale_load=demand*0.02, scale_pv=369)
+    data24 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=185)
+    data25 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=123)
+    data26 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=211)
+    data27 = ts_inputs(parameter, load='B90', scale_load=demand*0.02, scale_pv=345)
+    data28 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=125)
+    data29 = ts_inputs(parameter, load='B90', scale_load=demand*0.01, scale_pv=199)
+    data30 = ts_inputs(parameter, load='B90', scale_load=demand*0.14, scale_pv=2772)
+    data31 = ts_inputs(parameter, load='B90', scale_load=demand*0.02, scale_pv=506)
+    data32 = ts_inputs(parameter, load='B90', scale_load=demand*0.04, scale_pv=866)
+    data33 = ts_inputs(parameter, load='B90', scale_load=demand*0.06, scale_pv=1195)
+    data34 = ts_inputs(parameter, load='B90', scale_load=demand*0.09, scale_pv=1831)
+    data35 = ts_inputs(parameter, load='B90', scale_load=demand*0.02, scale_pv=449)
+    data36 = ts_inputs(parameter, load='B90', scale_load=demand*0.08, scale_pv=1594)
     # use data1 as starting point for multinode df
     data = data5.copy()
     # drop load and pv from multinode df
     data = data.drop(labels='load_demand', axis=1)
     data = data.drop(labels='generation_pv', axis=1)
-
-    # add node specifc load and pv (where applicable)
+    # demand node
     data['pf_demand_node4']  =  data5['load_demand']
     data['pf_demand_node5']  =  data5['load_demand']
     data['pf_demand_node6']  =  data6['load_demand']
@@ -103,8 +94,7 @@ def data_multinode(parameter):
     data['pf_demand_node34'] = data34['load_demand']
     data['pf_demand_node35'] = data35['load_demand']
     data['pf_demand_node36'] = data36['load_demand']
-
-
+    # generation node
     data['pf_pv_node4']  =  data4['generation_pv']
     data['pf_pv_node5']  =  data5['generation_pv']
     data['pf_pv_node6']  =  data6['generation_pv']
@@ -138,12 +128,9 @@ def data_multinode(parameter):
     data['pf_pv_node34'] = data34['generation_pv'] 
     data['pf_pv_node35'] = data35['generation_pv']
     data['pf_pv_node36'] = data36['generation_pv'] 
-
     return data
 
-if __name__ == '__main__':  
-    parameter = parameters()
-    data = data_multinode(parameter)
+def execute_solver(parameter, data):
     output_list = default_output_list(parameter)
     solver_path = "C:\\Nohora\\UniValle_project\\pasto_case\\DOPER\\doper\\solvers\\Windows64\\cbc.exe"
     smartDER = DOPER(model=control_model,
@@ -152,13 +139,41 @@ if __name__ == '__main__':
                      output_list=output_list)
     res = smartDER.do_optimization(data)
     duration, objective, df, model, result, termination, parameter = res
-    df.to_excel('./results/doperRes.xlsx', index=False)
-    df.to_csv('./results/doperRes.csv', index=False)
     print(standard_report(res))
+    return df
 
+def save_results_solver(df, i):
+    df.to_excel(f'./results/L2_3/doperRes{i}.xlsx', index=False)
+    df.to_csv(f'./results/L2_3/doperRes{i}.csv', index=False)
+
+def show_results_solver(df, i):
     plt.plot(df[['Import Power [kW]','PV Power [kW]', 'Load Power [kW]']])
     plt.title('Power flow at PCC')
     plt.legend(['Import Power [kW]','PV Power [kW]', 'Load Power [kW]'])
-    plt.show()
+    plt.save(f'./results/L2_3/Fig1_{i}.jpg')
+    plot_dynamic(df, parameter, plotFile = f'./results/L2_3/Fig2_{i}.jpg', plot_reg=False)
 
-    plotData = plot_dynamic(df, parameter, plotFile = None, plot_reg=False)
+if __name__ == '__main__':  
+    parameter = parameters()
+    demand = [8.9, 38.7, 582.5, 1155.6, 2989.8, 6340.0, 9108.8, 23724.3, 11865.5,
+             4629.7, 19548.7, 25972.5, 35441.8, 41558.6, 536.5, 2301.6, 139.3,
+             86847.3, 123598.7, 155795.6, 109602.5, 108342.6, 99047.5, 67926.1,
+             14709.8, 236133.7, 19783.1, 226017.5, 20115.6, 35732.1]
+    data_frames = []
+
+    print('Demand = ', demand[6], 'Año = ', 6)
+    data = data_multinode(parameter, demand[6])
+    df = execute_solver(parameter, data)
+    data_frames.append(df)
+    save_results_solver(df, 6)
+    show_results_solver(df, 6)
+    
+
+    # for i in range(len(demand)):
+    #     print('Demand = ', demand[i], 'Año = ', i)
+    #     data = data_multinode(parameter, demand[i])
+    #     df = execute_solver(parameter, data)
+    #     data_frames.append(df)
+    #     save_results_solver(df, i)
+    #     show_results_solver(df, i)
+    # #TODO: Correr para L1, L2y3, L4
